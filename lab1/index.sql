00a479947371f4bfcaf59cc7cbe3bbecaf8a6053
Whoisprivalov main computer <nikita.karatsev@gmail.com>
2019-06-27 23:35:05 +0600

    add first lab

--- lab1/index.sql
+++ lab1/index.sql
@@ -1,29 +1,30 @@
 -- TIMESTAMP: YYYYMMDD
-create table "PRODUCTS"(
+create table "PRODUCTS" (
       id_product number(5) primary key
     , name_product varchar2(30) NOT NULL
     , size_product varchar2(15)
     , weight_product number(5)
     )
 
-create table "PRODUCERS"(
+create table "PRODUCERS" (
       id_producer number(3) primary key
     , fullname_producer varchar2(60)
     , phone_number_producer number(15)
-    , adress_producer varchar(50)
     , country_producer varchar(20)
+    , adress_producer varchar(50)
     )
 
 -- надо вставить в супплайз id_...
-create table "SUPPLYS"(
+create table "SUPPLYS" (
       id_supply number(5) primary key
     , id_producer number(3) REFERENCES PRODUCERS(id_producer)
     , id_product varchar2(15) REFERENCES PRODUCTS(id_product)
-    , amount_product number(6) not null, price number(5)
+    , amount_product number(6) not null
+    , price number(5)
     , date_supplys TIMESTAMP(8)
     )
 
-create table "BUYER"
+create table "BUYER" (
       id_buyer number(5) primary key
     , full_name_buyer varchar2(60)
     , country_buyer varchar2(40) not null
@@ -31,12 +32,13 @@ create table "BUYER"
     , phone_number_buyer number(15)
     )
 
-create table "ORDERS"(
+create table "ORDERS" (
       id_order number(5) primary key
     , id_buyer number(5) not null
-    , id_supply number(5) not null
     , FOREIGN KEY (id_buyer) REFERENCES BUYER(id_buyer)
+    , id_supply number(5) not null
     , FOREIGN KEY (id_supply) REFERENCES SUPPLYS(id_supply)
     , amount_order number(5) not null
     , date_order TIMESTAMP(8)
     );
+    
